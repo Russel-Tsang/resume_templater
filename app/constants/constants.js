25cb4e6abacd5c8caf3c88c9f6da44b6e9b0600a
Russel <russel.tsang@baruchmail.cuny.edu>
2020-02-20 23:52:20 -0500

    create constants to reference throughout the application

---
+++ app/constants/constants.js
@@ -0,0 +1,23 @@
+export const ItemTypes = {
+    BLOCK: 'block'
+}
+
+export const BlockTypes = {
+    NAME: 'name',
+    CONTACT_INFO: 'contact-info',
+    SECTION_TITLE: 'section-title',
+    ITEM_TITLE_WITH_DATE: 'item-title-with-date',
+    ITEM_CAPTION: 'item-caption',
+    BULLET_POINT: 'bullet-point',
+    SPACE_BLOCK: 'space-block',
+}
+
+export const BlockStyles = {
+    'name': { height: '40px' },
+    'contact-info': { height: '40px' },
+    'section-title': { height: '25px' },
+    'item-title-with-date': { height: '22px' },
+    'item-caption': { height: '20px' },
+    'bullet-point': { height: '20px' }, 
+    'space-block': { height: '20px' },
+}
