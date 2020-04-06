7643ce9cc055a07a1a4a20d3cbf21ba692bd15ee
Russel <russel.tsang@baruchmail.cuny.edu>
2020-04-05 23:58:44 -0400

    add dragger div to define drag-state of block

--- app/components/wrappers/DragWrap.js
+++ app/components/wrappers/DragWrap.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDrag } from 'react-dnd';
 
 const DragWrap = Component => props => {
     const { itemType, onMouseDown } = props;
+    const [draggerState, setDraggerState] = useState(false);
 
     const [{ isDragging }, dragRef] = useDrag({
         item: { type: itemType },
         collect: monitor => ({ isDragging: !!monitor.isDragging() }),
+        canDrag: draggerState === true
     });
 
     // if component is being dragged, invoke the callback to change this state in index.js
-    // if (isDragging) props.setDraggingBlockIdx(props.idx);
+    // if (isDragging) props.setDraggingBlockIdx(props.idx);g
 
     return ( 
         <div ref={dragRef} className="drag-wrap" onMouseDown={onMouseDown}>
             <Component isDragging={isDragging} {...props}/>
+            <div 
+                className="dragger" 
+                onMouseDown={() => setDraggerState(true)} 
+                onMouseUp={() => setDraggerState(false)} 
+                style={{ height: '20px', width: '50px', backgroundColor: "red" }}
+            />
         </div>
     );
 }
