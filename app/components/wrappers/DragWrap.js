c3bdb1aec77253947b1699e079ee984206e75355
Russel <russel.tsang@baruchmail.cuny.edu>
2020-05-10 01:45:29 -0400

    edit onMouseDown event to focus() on input if input is event.target

--- app/components/wrappers/DragWrap.js
+++ app/components/wrappers/DragWrap.js
@@ -1,21 +1,57 @@
 import React, { useState } from 'react';
+// import { DragSource } from 'react-dnd';
 import { useDrag } from 'react-dnd';
+
+// const DragWrap = Component => props => {
+//     const { itemType, onMouseDown } = props;
+//     const [draggerState, setDraggerState] = useState(false);
+
+//     const [{ isDragging }, dragRef] = useDrag({
+//         item: { type: itemType },
+//         collect: monitor => ({ isDragging: !!monitor.isDragging() }),
+//         // canDrag: draggerState === true
+//     });
+
+//     // if component is being dragged, invoke the callback to change this state in index.js
+//     // if (isDragging) props.setDraggingBlockIdx(props.idx);
+
+//     return ( 
+//         <div ref={(e) => {console.log(dragRef); dragRef();}} className="drag-wrap" onMouseDown={onMouseDown}>
+//             <Component isDragging={isDragging} {...props}/>
+//             <div 
+//                 className="dragger" 
+//                 onMouseDown={() => setDraggerState(true)} 
+//                 onMouseUp={() => setDraggerState(false)} 
+//                 style={{ height: '20px', width: '50px', backgroundColor: "red" }}
+//             />
+//         </div>
+//     );
+// }
+ 
+// export default DragWrap;
 
 const DragWrap = Component => props => {
     const { itemType, onMouseDown } = props;
-    const [draggerState, setDraggerState] = useState(false);
+    // const type
 
     const [{ isDragging }, dragRef] = useDrag({
         item: { type: itemType },
         collect: monitor => ({ isDragging: !!monitor.isDragging() }),
-        canDrag: draggerState === true
+        // canDrag: draggerState === true
     });
 
-    // if component is being dragged, invoke the callback to change this state in index.js
-    // if (isDragging) props.setDraggingBlockIdx(props.idx);g
-
     return ( 
-        <div ref={dragRef} className="drag-wrap" onMouseDown={onMouseDown}>
+        <div 
+            ref={dragRef} 
+            className="drag-wrap" 
+            onMouseDown={(e) => {
+                console.log(e.target.className);
+                if (e.target.className === 'resume-input') {
+                    e.target.focus();
+                }
+                else onMouseDown();
+            }}
+        >
             <Component isDragging={isDragging} {...props}/>
             <div 
                 className="dragger" 
@@ -26,5 +62,5 @@ const DragWrap = Component => props => {
         </div>
     );
 }
- 
+
 export default DragWrap;
