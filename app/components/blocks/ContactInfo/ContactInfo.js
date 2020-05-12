d5a328a7b2a535c7f778107ac8bc0a2e5228569f
Russel <russel.tsang@baruchmail.cuny.edu>
2020-05-11 23:57:45 -0400

    add input elements with onChange handlers pointing to proper this.state.resume fields

--- app/components/blocks/ContactInfo/ContactInfo.js
+++ app/components/blocks/ContactInfo/ContactInfo.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const ContactInfo = (props) => {
     if (props.isDragging) return null;
+    const { address, email, phoneNumber } = props.contactInfo;
+
     return (
-        <div className="contact-info block" style={props.blockStyle}>
-            <span>{props.address}</span>
+        <div className={`${props.typeOfBlock} block`} style={props.blockStyle}>
+            {/* <span>{props.address}</span>
             <span>{props.email}</span>
-            <span>{props.phoneNumber}</span>
+            <span>{props.phoneNumber}</span> */}
+            <input value={address} onChange={(e) => props.onResumeEdit('address')(e.target.value)}/>
+            <input value={email} onChange={(e) => props.onResumeEdit('email')(e.target.value)}/>
+            <input value={phoneNumber} onChange={(e) => props.onResumeEdit('phoneNumber')(e.target.value)}/>
         </div>
     );
 }
--- app/components/blocks/ItemTitleWithDate/ItemTitleWithDate.js
+++ app/components/blocks/ItemTitleWithDate/ItemTitleWithDate.js
@@ -4,8 +4,10 @@ const ItemTitleWithDate = (props) => {
     if (props.isDragging) return null;
     return (
         <div className={`${props.typeOfBlock} block`} style={props.blockStyle}>
-            <span><b>{props.itemTitle}</b></span>
-            <span><i>{props.date}</i></span>
+            {/* <span><b>{props.itemTitle}</b></span>
+            <span><i>{props.date}</i></span> */}
+            <input className="item-title" value={props.itemTitle} onChange={(e) => props.onResumeEdit('itemTitle')(e.target.value)} />
+            <input className="date" value={props.date} onChange={(e) => props.onResumeEdit('date')(e.target.value)} />
         </div>
     );
 }
