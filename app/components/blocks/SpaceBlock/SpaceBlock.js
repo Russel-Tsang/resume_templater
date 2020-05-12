e3986161120102be2790480dee75a3f9c886ad75
Russel <russel.tsang@baruchmail.cuny.edu>
2020-05-11 23:58:12 -0400

    remove usage of props.value

--- app/components/blocks/SpaceBlock/SpaceBlock.js
+++ app/components/blocks/SpaceBlock/SpaceBlock.js
@@ -3,9 +3,7 @@ import React from 'react';
 const SpaceBlock = (props) => {
     if (props.isDragging) return null;
     return (
-        <div className={`${props.typeOfBlock} block`} style={props.blockStyle}>
-            {props.value}
-        </div>
+        <div className={`${props.typeOfBlock} block`} style={props.blockStyle} />
     );
 }
 
